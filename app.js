badcf1fece8f43671d85fcc2a9fc2c62c1522274
shaluks97 <46239857+shaluks97@users.noreply.github.com>
2019-08-22 12:48:17 +0530

    home(login),registration page set

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-Hi allwyen
+const Express = require('express');
+var app =new Express();
+app.set('view engine','ejs');
+
+app.use(Express.static(__dirname+"/public"));
+
+app.get('/',(req,res)=>{
+  res.render('index')
+});
+
+app.get('/registration',(req,res)=>{
+    res.render('registration')
+});
+
+
+app.listen(4000,()=>{
+    console.log('server running on 4000')
+
+});
